e41effee5c9021c56a6df36ec21008095ff96909
Agucarpar <agucp@outlook.es>
2021-06-16 17:39:44 +0200

    creating new header

---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
