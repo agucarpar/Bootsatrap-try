4a6d289459268f3488ccc38c1d159bb0bd289511
Agucarpar <agucp@outlook.es>
2021-06-16 17:39:30 +0200

    cleaning code

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<style scoped>
+
+</style>
+<template>
+  <div>
+    <p>Pacooo</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Header",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
