15132219f7f64ee2833c493e1d2c886ba5b4c55f
Agucarpar <agucp@outlook.es>
2021-06-23 17:55:41 +0200

    remove vuex try

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,7 +7,7 @@
 <template>
   <b-container class="background-red">
     <Header/>
-    <Record v-if="showDocument"/>
+    <Record/>
     <Document/>
 
   </b-container>
@@ -17,27 +17,16 @@
 import Header from '@/components/header/Header.vue'
 import Record from '@/components/tabs/Record.vue'
 import Document from '@/components/tabs/Document.vue'
-import {mapGetters} from '@/store/store.js'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
-  data(){
-    return{
-      showDocument: this.$store.state.isShowingRecordsComponent
-    }
-  },
+
   components: {
     Header,
     Record,
     Document,
-  },
-  computed: {
-    showRecordcomponent: this.$store.state.isShowingRecordsComponent,
-    ...mapGetters({
-      this.$store.state.isShowingRecordsComponent
-    })
   },
 }
 </script>
